57395e0ff298190eb35a72106b98633b40b9072e
Tyler <tylerthomasallen@gmail.com>
2019-02-25 12:00:30 -0800

    reverse string in place

---
+++ reverse_string.js
@@ -0,0 +1,17 @@
+const reverseString = (arr) => {
+  for (let i = 0; i < arr.length / 2; i++) {
+    const firstTemp = arr[i];
+    const lastTemp = arr[arr.length - 1 - i];
+    arr[i] = lastTemp;
+    arr[arr.length - 1 - i] = firstTemp
+  }
+  return arr;
+}
+
+const test = ['a', 'b', 'c', 'd', 'f', 'g'];
+// ['d', 'c', 'b', 'a']
+
+console.log(reverseString(test));
+
+
+
